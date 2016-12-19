149eac3aedc98bd91027cdeb5e1af1a281c9a081
lgybetter <437675103@qq.com>
2016-12-19 23:54:10 +0800

    Finish the app.js config

--- app.js
+++ app.js
@@ -4,20 +4,12 @@ import http from 'http'
 import path from 'path' 
 import routes from './routes/index' 
 import config from './config' 
-import User from './models/user'
-import Book from './models/book' 
-const db = mysql.createConnection(config.mysql) 
-db.connect() 
+
+const db = mysql.createConnection(config.mysql)
+db.connect()
 const app = express() 
 const router = express.Router() 
 
-const book = new Book()
-book.queryAllBooks().then(info => console.log(info))
-// let c = 2;
-// user.queryAllUsers().then(info => {
-//   c = info; 
-//   console.log(c)
-// })
 app.set('port', process.env.PORT || config.app.port) 
 app.use(router) 
 app.use(express.static(path.join(__dirname, 'public'))) 
